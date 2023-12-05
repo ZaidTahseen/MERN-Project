b3a403429f278cde058b2aca03ad82e79bd35318
Tahseen Zaid <ansarizaid6410@gmail.com>
2023-12-06 00:15:41 +0530

    Setup passportjs with google-auth

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get('/', (req , res ) => {
-    res.send( { message : "From express"})
-})
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log("accessToken -> ", accessToken);
+    }
+  )
+);
 
+app.get("/", (req, res) => {
+  res.send({ message: "From express" });
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
-app.listen( PORT)
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
